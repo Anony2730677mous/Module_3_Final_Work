e483002cf8ba55d06891bf797c4d4601a62725fc
Anony2730677mous <104105@mail.ru>
2023-01-04 18:34:02 +0300

    v6 correction of test classes

--- src/test/Game/mechanica/GameLogicTest.java
+++ src/test/Game/mechanica/GameLogicTest.java
@@ -1,6 +1,5 @@
-package game;
+package game.mechanica;
 
-import game.mechanica.GameLogic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
